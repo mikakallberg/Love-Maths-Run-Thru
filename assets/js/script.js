52eb810342eb76693a346e8c1c7def42063af3fd
mikakallberg <mikaela.kallberg@hotmail.com>
2022-04-12 12:03:12 +0000

    Add eventlistener, stating focus and box refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 
 
@@ -25,6 +31,16 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 function runGame(gameType){
+
+    /**
+     * emptying box when refresh
+     */
+    document.getElementById("answer-box").value = "";
+    /**
+     * stating focus
+     */
+    document.getElementById("answer-box").focus();
+
     //creates random numbers between 1 and 25
 let num1 = Math.floor(Math.random() *25) + 1;
 let num2 = Math.floor(Math.random() *25) + 1;
