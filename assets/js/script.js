17d1c0280c6cb4b59dc5d2327a56510063ba92e2
mikakallberg <mikaela.kallberg@hotmail.com>
2022-04-12 11:48:06 +0000

    Add code for subtract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,9 @@ let num2 = Math.floor(Math.random() *25) + 1;
 
 if (gameType === "addition"){
     displayAdditionQuestion(num1, num2);
+}else if (gameType === "subtract"){
+    displaySubtractQuestion(num1, num2);
+
 }else if (gameType === "multiply"){
     displayMultiplyQuestion(num1, num2);
 
@@ -72,6 +75,9 @@ function calculateCorrectAnswer(){
 
     if (operator === "+"){
         return[operand1 + operand2, "addition"];
+    }else if (operator === "-"){
+        return [operand1 - operand2, "subtract"]
+
     }else if (operator === "x"){
         return [operand1 * operand2, "multiply"]
 
@@ -106,7 +112,10 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 
-function displaySubtractQuestion(){
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
